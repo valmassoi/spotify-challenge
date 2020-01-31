063a47495de2bcb2b7ba0e71bbcc553d9baf69af
Robert Valmassoi <valmassoi@protonmail.com>
2020-01-31 14:18:53 -0500

    sortByCount

--- index.js
+++ index.js
@@ -20,19 +20,43 @@ export const buildCharacterCount = (paragraph, caseSensitive) => {
 }
 
 /**
+ * we want the most uniq. chars to be removed
+ * we want the most uniq. chars to be removed
+ * @param {object} countOfEachChar 
+ * @return {array<object>}
+ */
+export const sortByCount = (countOfEachChar) => {
+  let array = [];
+  for (var character in countOfEachChar) {
+    array.push({ character, count: countOfEachChar[character]});
+  }
+  return array.sort((a, b) => a.count - b.count);
+}
+
+/**
  * program outputs the largest unique set of characters
  * that can be removed from this paragraph
  * without letting its length drop below 50.
  * @param {string} paragraph input string
  * @param {number=50} minLength minimum length after removing letters
  * @param {bool=true} caseSensitive
- * @return {array} uniqSet
+ * @return {array} uniqCharsToRemove largest unique set of characters that can be removed from this paragraph without letting its length drop below the minLength, answer to the prompt
  */
 export const app = (paragraph, minLength = 50, caseSensitive = true) => {
+  const uniqCharsToRemove = [];
   if (!paragraph || minLength <= paragraph.length) {
-    return [];
+    return uniqCharsToRemove; // empty array, nothing to remove
   }
   const countOfEachChar = buildCharacterCount(paragraph, caseSensitive);
-  // TODO
-  return;
+  const countOfEachCharSorted = sortByCount(countOfEachChar)
+  const amountToBeRemoved = paragraph.length - minLength;
+  let indexToRemove = 0;
+  let amountRemoved = 0;
+  while (amountRemoved < amountToBeRemoved) {
+    const { character: charBeingRemoved, count: amountBeingRemoved } = countOfEachCharSorted[indexToRemove];
+    uniqCharsToRemove.push(charBeingRemoved)
+    indexToRemove++;
+    amountRemoved += amountBeingRemoved;
+  }
+  return uniqCharsToRemove;
 };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { app, buildCharacterCount } from './index';
+import { app, buildCharacterCount, sortByCount } from './index';
 
 const promptParagraph = `If you want to jumpstart the process of talking to us about this role, here’s a little challenge: write a program that outputs the largest unique set of characters that can be removed from this paragraph without letting its length drop below 50.`;
 
@@ -32,9 +32,22 @@ describe('spotify challenge', () => {
       });
     });
   });
+  describe('sortByCount', () => {
+    it('sorts by count, building array of objects', () => {
+      expect(sortByCount({ h: 2, e: 1, y: 1, '!': 5})).toEqual([
+        { character: 'e', count: 1 },
+        { character: 'y', count: 1 },
+        { character: 'h', count: 2 },
+        { character: '!', count: 5 },
+      ]);
+    })
+  })
   describe('app', () => {
-    it('returns empty array', () => {
+    it('returns empty array with no supplied paragraph', () => {
       expect(app('')).toEqual([]);
+    });
+    it('returns empty array if the minLength is less than or equal paragraph length', () => {
+      expect(app('Hey!', 4)).toEqual([]);
     });
   });
 });
